c9e6ad5cadcfd96feccf3bcf24ba7922758e5bb1
xiaoxiae <tomas@slama.dev>
2021-04-17 12:30:18 +0200

    implement ISpriteSets

--- Src/Assets/Sprites.cs
+++ Src/Assets/Sprites.cs
@@ -2,16 +2,42 @@ using System;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 
-namespace yolo {
-    public class TimedSpriteSet {
+namespace yolo
+{
+    public interface ISpriteSet
+    {
         public bool IsFlat { get; }
-        public Sprite GetSpriteAt(int millis) {
-            throw new NotImplementedException();
+        public bool Loops { get; }
+        public Sprite GetSpriteAt(int millis);
+    }
+
+    public class SimpleSpriteSet : ISpriteSet
+    {
+        private Sprite Sprite { get; init; }
+        public bool IsFlat { get; init; }
+        public bool Loops { get; } = false;
+
+        public Sprite GetSpriteAt(int millis) => Sprite;
+    }
+
+    class TimedSpriteSet : ISpriteSet
+    {
+        public Sprite[] Sprites { get; init; }
+        public bool IsFlat { get; init; }
+        public bool Loops { get; init; }
+        public int Period { get; init; }
+        
+        public Sprite GetSpriteAt(int millis = 500)
+        {
+            int frame = (millis / Period) % Sprites.Length;
+            return Sprites[frame];
         }
     }
 
-    public class Sprite {
+    public class Sprite
+    {
         public Texture2D Texture { get; init; }
+        public SpriteEffects Effects { get; init; } = SpriteEffects.None;
         public Rectangle SourceRect { get; init; }
         public Vector2 Origin { get; init; } = Vector2.Zero;
         public Color Tone { get; init; } = Color.White;
