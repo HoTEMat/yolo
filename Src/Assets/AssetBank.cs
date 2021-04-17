78da607b0d46a06a3d9fddbbe310a6ad993aca2a
Jan Kytka <kytka.honza@gmail.com>
2021-04-17 10:06:37 +0200

    content load example

--- Src/Assets/AssetBank.cs
+++ Src/Assets/AssetBank.cs
@@ -1,7 +1,26 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+
 namespace yolo {
     public class AssetBank {
-        public void LoadContent() {
-            
+
+        public class TextureBank {
+            public Texture2D Test { get; set; }
+        }
+
+        public class Sprites {
+
+        }
+
+        public TextureBank Textures { get; private set; }
+
+        public void LoadContent(ContentManager Content) {
+            Textures = new TextureBank();
+
+            Textures.Test = Content.Load<Texture2D>("assetName");
+
+
         }
     }
 }
