446bd670aa4f8d0679e389a4bf0e79077e4906e1
Matěj Kripner <kripnermatej@gmail.com>
2021-04-17 10:49:52 +0200

    Cleanup

--- Src/Game/World.cs
+++ Src/Game/World.cs
@@ -1,11 +1,16 @@
 using System.Collections.Generic;
+using Microsoft.Xna.Framework;
 
 namespace yolo {
     public class World {
         public List<Scene> Scenes { get; }
         public Scene CurrentScene { get; }
+        private Context context;
+        
+        public World(Context context) =>
+            this.context = context;
 
-        public void SwitchToScene(Scene nextScene) {
+        public void SwitchToScene(Scene nextScene, Vector2 playerPosition) {
             
         }
     }
