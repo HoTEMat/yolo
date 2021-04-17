725aa87eb7fee3c5eb7af605f9d2dc1869ab753f
Jan Kytka <kytka.honza@gmail.com>
2021-04-17 11:15:45 +0200

    dev asset bank

--- Src/Assets/AssetBank.cs
+++ Src/Assets/AssetBank.cs
@@ -1,35 +1,59 @@
-using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Graphics;
-using Microsoft.Xna.Framework.Content;
+using ContentManager = Microsoft.Xna.Framework.Content.ContentManager;
+using Texture2D = Microsoft.Xna.Framework.Graphics.Texture2D;
+using Rectangle = Microsoft.Xna.Framework.Rectangle;
+using Vector2 = Microsoft.Xna.Framework.Vector2;
+using Color = Microsoft.Xna.Framework.Color;
 
 namespace yolo {
     public class AssetBank {
 
-        ContentManager content;
+        private ContentManager content;
 
-        public TextureBank Textures { get; } = new TextureBank();
+        public TextureBank Textures { get; private set; }
         public class TextureBank {
-            public Texture2D Dev { get; private set; }
-
-            public void LoadContent(AssetBank assets) {
-
-            }
+            public Texture2D Dev { get; init; }
         }
 
-        public SpriteBank Sprites { get; } = new SpriteBank();
+        public SpriteBank Sprites { get; private set; }
         public class SpriteBank {
-            public Sprite RoadSprite = new Sprite();
+            public Sprite Dev_Wall { get; init; }
+            public Sprite Dev_Floor { get; init; }
+        }
 
-            public void LoadContent(AssetBank assets) {
-
-            }
+        public TileBank Tiles { get; private set; }
+        public class TileBank {
+            public Tile Dev_Wall { get; init; }
+            public Tile Dev_Floor { get; init; }
         }
 
 
         public void LoadContent(ContentManager Content) {
-            Textures = new TextureBank();
 
-            Textures.Test = Content.Load<Texture2D>("assetName");
+            // Load textures
+            Textures = new TextureBank {
+                Dev = Content.Load<Texture2D>("assetName"),
+            };
+
+            Sprites = new SpriteBank {
+                Dev_Floor = new Sprite {
+                    Texture = Textures.Dev
+                },
+                Dev_Wall = new Sprite {
+                    Texture = Textures.Dev,
+                    SourceRect = new Rectangle(0, 32, 16, 32),
+                },
+            };
+
+            Tiles = new TileBank {
+                Dev_Wall = new Tile {
+                    Sprite = Sprites.Dev_Wall,
+                    Walkable = false,
+                },
+                Dev_Floor = new Tile {
+                    Sprite = Sprites.Dev_Floor,
+                    Walkable = true,
+                },
+            };
 
 
         }
